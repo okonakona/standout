501f3b407ee8e0019cf677bd4e2c3f414ee25e35
UTakuto <taku.0307.ue@icloud.com>
2025-12-12 11:58:04 +0900

    refactor: public内のsvg icon削除、svg iconをIcons.tsxに統合

--- src/components/svg/index.ts
+++ src/components/svg/index.ts
@@ -1,12 +1,1 @@
-export { default as ColorPointSvg } from "./ColorPointSvg";
-export { default as LipPointSvg } from "./LipPointSvg";
-
-// 便利な型定義もエクスポート
-export type SvgProps = {
-    className?: string;
-    style?: React.CSSProperties;
-    fillColor?: string;
-    strokeColor?: string;
-    width?: number;
-    height?: number;
-};
+export * from "./Icons";
