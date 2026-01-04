fb417bc3d04d2e1e40e7c79d84d0401a2d4081ce
okonakona <2240057@ecc.ac.jp>
2026-01-05 05:36:10 +0900

    fix: シミュレーションへのボタン配置とhtmlの調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,33 @@ import Footer from "@/components/footer/page";
 import Header from "@/components/header/page";
 import Image from "next/image";
 import Link from "next/link";
+import SimulatePage from './simulate/page';
 
 export default function Home() {
     return (
         <>
             <Header />
             <main>
-                <div className={styles.imgWrap}>
-                    <Image src="/assets/sample.png" alt="サンプル" width={390} height={241} />
+                <div className={styles.content}>
+                    <h1 className={styles.imgWrap}>
+                        <Image src="/assets/sample.png" alt="サンプル" width={390} height={241} />
+                    </h1>
+                    <button className={styles.button}>メイクシミュレーション</button>
+                    <section>
+                        <h2>スタイルガイド</h2>
+                        <div className={styles.guidWrap}>
+                            {/* <Link href="/">
+                                <Image src="/assets/sample.png" alt="サンプル" width={390} height={241} />
+                                <p>綺麗系</p>
+                            </Link> */}
+                        </div>
+                    </section>
+                    <section>
+                        <h2>イケメンメイクコラム</h2>
+                        <div className={styles.articleWrap}>
+                        </div>
+                    </section>
                 </div>
-                <h1><Link href="/simulate">メイクシミュレーション</Link></h1>
-                <h2>スタイルガイド</h2>
-                <h2>イケメンメイクコラム</h2>
                 <div className={styles.modalCnt}>
                 </div>
             </main>
