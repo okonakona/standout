f64df484c050b478b7890f9885db461394e80d96
UTakuto <taku.0307.ue@icloud.com>
2026-02-04 00:19:03 +0900

    feat: フッターコンポーネントのリンクと画像要素の整形を改善

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -1,21 +1,51 @@
-import Link from 'next/link'
-import styles from './style.module.css'
-import Image from 'next/image'
-import CameraButton from '../cameraButton/page'
+import Link from "next/link";
+import styles from "./style.module.css";
+import Image from "next/image";
+import CameraButton from "../cameraButton/page";
 
-export default function Footer () {
-    return(
+export default function Footer() {
+    return (
         <footer>
-                <CameraButton />
+            <CameraButton />
             <div className={styles.content}>
                 <nav>
                     <ul>
-                        <li><Link href="/"><Image src="/assets/heartIcon.svg" width={33} height={33} alt='ハートのアイコン'  />お気に入り</Link></li>
-                        <li><Link href="/"><Image src="/assets/homeIcon.svg" width={33} height={33} alt='家のアイコン' />ホーム</Link></li>
-                        <li><Link href="/"><Image src="/assets/peopleIcon.svg" width={33} height={33} alt='人型のアイコン' />マイページ</Link></li>
+                        <li>
+                            <Link href="/">
+                                <Image
+                                    src="/assets/heartIcon.svg"
+                                    width={33}
+                                    height={33}
+                                    alt="ハートのアイコン"
+                                />
+                                お気に入り
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/">
+                                <Image
+                                    src="/assets/homeIcon.svg"
+                                    width={33}
+                                    height={33}
+                                    alt="家のアイコン"
+                                />
+                                ホーム
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/mypage">
+                                <Image
+                                    src="/assets/peopleIcon.svg"
+                                    width={33}
+                                    height={33}
+                                    alt="人型のアイコン"
+                                />
+                                マイページ
+                            </Link>
+                        </li>
                     </ul>
                 </nav>
             </div>
         </footer>
-    )
+    );
 }
